0e1aa1afdbf1dea9d2c5312b7fee1bab98defe1c
In5perat0r <schlacht.mike@gmail.com>
2025-05-13 11:15:19 +0200

    C

--- test/script.js
+++ test/script.js
@@ -1,5 +1,5 @@
 addEventListener('DOMContentLoaded', e => {
-    fetch('https://in5perat0r.github.io/test/content/').then(response => {
+    fetch('https://in5perat0r.github.io/test/index.json').then(response => {
         console.log(response);
     })
 })
