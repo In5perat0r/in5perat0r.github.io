1772d6195688c0d3feaef0c76eb4680d22c49b16
In5perat0r <schlacht.mike@gmail.com>
2025-05-13 11:25:32 +0200

    Change Date: 05/13/2025 - 11:25

--- test/script.js
+++ test/script.js
@@ -1,5 +1,19 @@
+function appendURLPath(url, value) {
+    let u = new URL(url);
+    u.pathname = u.pathname + value;
+    return u.toString();
+}
+
 addEventListener('DOMContentLoaded', e => {
-    fetch('https://in5perat0r.github.io/test/index.json').then(response => {
-        console.log(response);
+    'https://in5perat0r.github.io/test/content/New%20Text%20File%20(1).txt'
+    let baseURL = 'https://in5perat0r.github.io/test';
+    fetch(baseURL + '/index.json').then(response => {
+        response.json().then(content => {
+            let ls = [];
+            content.forEach(v => {
+                ls.push(appendURLPath(baseURL, '/content/' + v));
+            });
+            console.log(ls);
+        })
     })
 })
