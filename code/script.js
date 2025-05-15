ceab557f2eadcb9975bc78c7c487048ba6a64638
In5perat0r <schlacht.mike@gmail.com>
2025-05-15 13:06:17 +0200

    Change Date: 05/15/2025 - 13:06

--- code/script.js
+++ code/script.js
@@ -1,6 +1,6 @@
 addEventListener('DOMContentLoaded', event => {
     document.querySelectorAll('pre[script] > code').forEach(item => {
-        let name = item.getAttribute('script');
+        let name = item.parentElement.getAttribute('script');
         fetch(`https://in5perat0r.github.io/userscripts/${name}`).then(response => {
             response.text().then(txt => {
                 let extension = name.substring(name.lastIndexOf('.')+1);
