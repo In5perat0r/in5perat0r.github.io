1673ad90095f31acc9cdaccb24a41fe0f0c08353
In5perat0r <schlacht.mike@gmail.com>
2025-05-15 12:53:50 +0200

    Change Date: 05/15/2025 - 12:53

---
+++ code/script.js
@@ -0,0 +1,12 @@
+addEventListener('DOMContentLoaded', event => {
+    document.querySelectorAll('.script-injection').forEach(item => {
+        let name = item.getAttribute('script');
+        fetch(`https://in5perat0r.github.io/userscripts/${name}`).then(response => {
+            response.text().then(txt => {
+                let extension = name.substring(name.lastIndexOf('.')+1);
+                item.innerHTML = txt;
+                item.classList.add(`language-${extension}`);
+            })
+        })
+    })
+})
---
+++ userscripts/test.js
@@ -0,0 +1,10 @@
+// Test Code Block
+function test(value, value2, value3) {
+    return "TTT" + value;
+}
+
+var value1 = "Test";
+value.split(',');
+var value2 = 1;
+let value3 = true;
+const value4 = new URL("https://www.google.com/");
